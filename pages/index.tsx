3284b59b4d584e22b6d752848a1cab251501234b
0xGGCrypto <90081002+0xGGCrypto@users.noreply.github.com>
2021-09-05 00:56:03 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,7 +98,7 @@ const IndexPage = ({ robes, lastUpdate }: Props) => {
       <h1 className="text-lg md:text-3xl">1337 Gang</h1>
       <div className="text-center max-w-screen-md md:leading-loose">
         <p className="md:text-xl">
-          There are {robes.length} Gear Stash's for sale with Punk <strong>1337 gear</strong>. The floor
+          There are {robes.length} Gear Stash's for sale with Punk <strong>+1 gear</strong>. The floor
           price is {robes[0].price} ETH.
         </p>
         <p className="md:text-lg pt-2">
